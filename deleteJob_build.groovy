e672db168a21aef3dd76f0c0078d4a86fcad4b34
Ubuntu <ubuntu@ip-172-31-2-89.us-east-2.compute.internal>
2021-07-23 18:29:52 +0000

    final

---
+++ deleteJob_build.groovy
@@ -0,0 +1,40 @@
+import jenkins.model.*
+import jenkins.model.Jenkins
+import hudson.model.Job
+
+def deleteJob(deleteRegex) {
+    def deleteJobPat = deleteRegex
+    def matchedJobs = Jenkins.instance.items.findAll { job ->
+        job.name =~ /${deleteJobPat}/
+}
+matchedJobs.each { job ->
+    job.delete()
+}
+}
+
+
+def deleteBuild() {
+minimum_builds = 3
+
+for (job in Jenkins.instance.items) {
+  println job.name
+
+  def recent = job.builds.limit(minimum_builds)
+  
+  for (build in job.builds) {
+    if (!recent.contains(build)) {
+	  def build_time = build.getTime()
+	  use (groovy.time.TimeCategory) {
+		def current_date = new Date()
+		def duration = current_date - build_time
+        if(duration.hours>4) {
+          println "deleting  - ${build} , it is ${duration.hours} hours old"
+          build.delete()
+        }
+      }
+    }
+  }
+}
+
+}
+return this
